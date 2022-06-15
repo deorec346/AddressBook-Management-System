df240f1f55e761a347d6991b19de6f27d7673488
Chetan Deore <97680266+deorec346@users.noreply.github.com>
2022-06-16 02:23:27 +0530

    Update addressbook_management_system.py

--- addressbook_management_system.py
+++ addressbook_management_system.py
@@ -28,12 +28,30 @@ class AddressBook:
     def get_contact(self, first_name):
         return self.contact_dict.get(first_name)
 
+    def edit_contact(self, contact_obj, contact_dict):
+        contact_obj.first_name = contact_dict.get("first_name")
+        contact_obj.last_name = contact_dict.get("last_name")
+        contact_obj.city = contact_dict.get("city")
+        contact_obj.state = contact_dict.get("state")
+        contact_obj.zip = contact_dict.get("zip")
+        contact_obj.phone = contact_dict.get("phone")
+        contact_obj.email = contact_dict.get("email")
+
     def display_data_string(self):
         print("---------------------------------------------------------------------------------------")
         print("First\tLast\tPhone\taddress\tCity \tState\tzip code")
         print("---------------------------------------------------------------------------------------")
         for v in self.contact_dict.items():
             print(f"{v.first_name}\t{v.last_name}\t{v.phone}\t{v.address} \t{v.city}\t{v.state}\t{v.zip}")
+
+    def delete_contact(self, contact_name):
+        contact_obj = self.get_contact(contact_name)
+        if not contact_obj:
+            print("Name not found")
+            return
+        # print(type(book_obj))
+        del self.contact_dict[contact_name]
+        print("contact deleted")
 
 
 def add_addressbook_name():
@@ -94,6 +112,67 @@ def add_contact():
         print("Please enter correct Address book name", e)
 
 
+def edit_contact():
+    """
+    Method to edit existing contact
+    :return:
+    """
+    try:
+        addressbook_name = input("Enter Address Book Name")
+        book_obj = addressbook_dict.get(addressbook_name)
+        if not book_obj:
+            print("Book not found")
+            return
+        first_name = input("Enter the first name you want to edit")
+        contact_obj = book_obj.get_contact(first_name)
+        if not contact_obj:
+            print("Name not found")
+            return
+
+        last_name = input("Enter last name \n")
+        city = input("Enter city \n")
+        state = input("Enter state \n")
+        zip = input("Enter zip code \n")
+        phone = input("Enter phone number \n")
+        email = input("Enter email address \n")
+
+        """
+        1 = value if condition is True
+        2 = value if condition is False
+        1 if "condition" else 2 
+        
+        """
+        data_dict = {"first_name": first_name,
+                     "last_name": last_name if last_name != "" else contact_obj.last_name,
+                     "city": city if city != "" else contact_obj.city,
+                     "state": state if state != "" else contact_obj.state,
+                     "zip": zip if zip != "" else contact_obj.zip,
+                     "phone": phone if phone != "" else contact_obj.phone,
+                     "email": email if email != "" else contact_obj.email}
+
+        book_obj.edit_contact(contact_obj, data_dict)
+        print("successfully contact updated")
+    except Exception as e:
+        print(" You are entering wrong book name", e)
+
+
+def delete_contact():
+    """
+    Method to delete contact from Addressbook
+    :return:
+    """
+    try:
+        addressbook_name = input("Enter Address Book Name : ")
+        book_obj = addressbook_dict.get(addressbook_name)
+        if not book_obj:
+            print("Book not found")
+            return
+        first_name = input("Enter the first name you want to delete : ")
+        book_obj.delete_contact(first_name)
+    except Exception as e:
+        print(" Please enter valid input", e)
+
+
 def display_contact():
     """
     Method to Displaying the contact are present in specific Addressbook.
@@ -119,8 +198,8 @@ if __name__ == '__main__':
               "5. Edit contact\n"        "6. Display contact\n"
               "0. Exit address book...")
         choice = {1: add_addressbook_name, 2: display_address_book,
-                  3: add_contact,
-                  4: display_contact}
+                  3: add_contact, 4: delete_contact, 5: edit_contact,
+                  6: display_contact}
         try:
             user_input = int(input("Enter choice: "))
             if user_input != 0:
